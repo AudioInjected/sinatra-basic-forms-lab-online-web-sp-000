f336d011e9ba67cd9679d56b6a8eae1e36ae0863
Rafael Batista <rafaelb24@yahoo.com>
2019-02-15 01:19:13 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,9 +2,10 @@ class Puppy
   attr_accessor :name, :age
   attr_reader :breed
   
-  def initialize(attributes)
+  def initialize(attributes = nil)
     @breed = attributes[:breed]
     @age = attributes[:age]
     @name = attribites[:name]
+    binding.pry
   end 
 end 
