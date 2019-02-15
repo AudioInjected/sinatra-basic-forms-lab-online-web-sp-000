558771f24151fa53c3c590dc4d3ee9ed41a18ecf
Rafael Batista <rafaelb24@yahoo.com>
2019-02-15 01:25:50 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,13 +2,18 @@ class Puppy
   attr_accessor :name, :months_old
   attr_reader :breed
   
-  def initialize(breed, months_old, name)
-    @breed = breed
-    @age = months_old
-    @name = name
+  def initialize(attributes)
+    #@breed = breed
+   # @age = months_old
+    #@name = name
+        @breed = attributes[:breed]
+    @age = attributes[:months_old]
+    @name = attributes[:name]
   end 
 end 
 
 attributes = {}
 
-@bree
+    @breed = attributes[:breed]
+    @age = attributes[:months_old]
+    @name = attributes[:name]
